3ca88e100b140e9ea1f40e62842b921c85fc69e5
C.P Williams <conor.williams@gmail.com>
2025-09-15 22:37:47 +0100

    1.2

---
+++ ALL.2024/2024.day1.2.hs
@@ -0,0 +1,95 @@
+import qualified Data.Vector as V
+import System.Environment (getArgs)
+import Data.Char (isDigit)
+import Data.Maybe (fromMaybe)
+import Control.Monad (forM_)
+import Control.Monad (when)
+--import System.Exit (exitWith, ExitCode(..))
+import Control.Monad.Cont
+import Data.List
+import Data.List (isPrefixOf)
+import Data.List (nub)
+--import Data.IORef
+--import System.IO.Unsafe (unsafePerformIO)
+--import Control.Monad.ST
+--import Data.STRef
+
+
+loopThrough :: [String] -> Int -> [Int] -> [Int] -> ([Int], [Int])
+loopThrough [] pos123 left right = (left, right)
+loopThrough (str:vv) pos123 left right = do
+    let amt = extractInt str
+    let amt2 = convertMaybeToInt amt
+    if (pos123 `mod` 2) == 0 then do
+        let left2 = left ++ [amt2]
+        let ans = loopThrough vv (pos123 + 1) left2 right
+        --(left2, right)
+        ans
+    else do
+        let right2 = right ++ [amt2]
+        let ans = loopThrough vv (pos123 + 1) left right2
+        --(left, right2)
+        ans
+
+loop2 :: [Int] -> [Int] -> Int -> Int -> Int
+loop2 [] vv2 pos1 tot = tot
+loop2 (xx:vv) vv2 pos1 tot = do
+    let tot2 = tot + (abs (xx - (vv2 !! pos1)))
+    let ans = loop2 vv vv2 (pos1+1) tot2
+    ans
+    
+
+firstDigit :: Int -> Int
+firstDigit n = read [head (show (abs n))] :: Int
+
+extractInt :: String -> Maybe Int
+extractInt str =
+    let digits = filter isDigit str
+    in if null digits then Nothing else Just (read digits :: Int)
+
+convertMaybeToInt :: Maybe Int -> Int
+convertMaybeToInt maybeInt = fromMaybe 0 maybeInt
+
+    
+main :: IO ()
+main = do
+    putStrLn "\t\t2024 day1.2"
+    putStrLn "\t/ghc -package mtl xx.hs"
+
+    args <- getArgs
+    case args of
+        [filename] -> do
+            content <- readFile filename
+            let yy = replaceSubstring "   " "\n" content
+            let v1 = concat (replicate 1 (lines yy))
+            --print (lines yy)
+            putStr "h_ans: "
+            let (le, ri) = loopThrough v1 0 [] []
+            let le1 = sort le
+            let ri1 = sort ri
+            --let leuniq = nub le1
+            --let riuniq = nub ri1
+            --let xx = loop2 le1 ri1 0 0
+            let zz = loopleft le1 ri1 0
+            print zz
+
+loopleft :: [Int] -> [Int] -> Int -> Int
+loopleft [] ri1 tot = tot
+loopleft (el:le1) ri1 tot = do
+    let xx = loopOccur ri1 el
+    let tot2 = tot + (xx * el)
+    let qq = loopleft le1 ri1 tot2
+    qq
+     
+loopOccur :: [Int] -> Int -> Int
+loopOccur xs n = length [x | x <- xs, x == n] 
+
+--countOccurrences :: Int -> [Int] -> Int
+--countOccurrences n xs = length [x | x <- xs, x == n]
+
+replaceSubstring :: String -> String -> String -> String
+replaceSubstring _ _ [] = []
+replaceSubstring old new str
+  | old `isPrefixOf` str = new ++ replaceSubstring old new (drop (length old) str)
+  | otherwise            = head str : replaceSubstring old new (tail str)
+
