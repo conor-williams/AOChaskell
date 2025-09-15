3bd84de2ef7d712a2c23f44b1d5ac62661afd1f1
C.P Williams <conor.williams@gmail.com>
2025-09-15 22:36:32 +0100

    1.2

---
+++ ALL.2021/2021.day1.2.hs
@@ -0,0 +1,64 @@
+import qualified Data.Vector as V
+import System.Environment (getArgs)
+import Data.Char (isDigit)
+import Data.Maybe (fromMaybe)
+import Control.Monad (forM_)
+import Control.Monad (when)
+--import System.Exit (exitWith, ExitCode(..))
+import Control.Monad.Cont
+import Data.List
+--import Data.IORef
+--import System.IO.Unsafe (unsafePerformIO)
+--import Control.Monad.ST
+--import Data.STRef
+
+
+loopThrough :: [String] -> [String] -> Int -> Int -> Int
+--loopThrough [] list1 cur1 = return -9999
+loopThrough [] list1 cnt pos = cnt
+loopThrough (str:vv) list1 cnt pos = do
+    let amt = extractInt str
+    let amt2 = convertMaybeToInt amt
+    let sec = extractInt (list1 !! pos)
+    let sec2 = convertMaybeToInt sec
+    let thi = extractInt (list1 !! (pos+1))
+    let thi2 = convertMaybeToInt thi
+    let fou = extractInt (list1 !! (pos+2))
+    let fou2 = convertMaybeToInt fou
+
+    if (pos+2) == (length list1) then do
+        cnt
+    else do 
+        let gr1 = amt2 + sec2 + thi2
+        let gr2 = sec2 + thi2 + fou2
+	if gr2 > gr1 then do
+	    let ans = loopThrough vv list1 (cnt+1) (pos+1)
+	    ans
+	else do
+	    let ans = loopThrough vv list1 (cnt) (pos+1)
+	    ans
+
+extractInt :: String -> Maybe Int
+extractInt str =
+    let digits = filter isDigit str
+    in if null digits then Nothing else Just (read digits :: Int)
+
+convertMaybeToInt :: Maybe Int -> Int
+convertMaybeToInt maybeInt = fromMaybe 0 maybeInt
+
+    
+main :: IO ()
+main = do
+    putStrLn "\t\t2021 day1.2"
+    putStrLn "\t/ghc -package mtl xx.hs"
+
+    args <- getArgs
+    case args of
+        [filename] -> do
+            content <- readFile filename
+            let v1 = concat (replicate 1 (lines content))
+            --print v20
+            putStr "h_ans: "
+            --countUp3 v
+            --print $ loopThrough v1 (reverse v1) -1
+            print $ loopThrough v1 v1 0 1
