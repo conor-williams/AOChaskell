83e76ea8a423d327fd234a3d163b17b50133a7d3
C.P Williams <conor.williams@gmail.com>
2025-09-15 23:14:52 +0100

    day1

--- ALL.2020/2020.day1.2.hs
+++ ALL.2020/2020.day1.2.hs
@@ -95,7 +95,6 @@ convertMaybeToInt maybeInt = fromMaybe 0 maybeInt
 main :: IO ()
 main = do
     putStrLn "\t\t2020 day1.2"
-    putStrLn "\t/ghc -package mtl xx.hs"
 
     args <- getArgs
     case args of
